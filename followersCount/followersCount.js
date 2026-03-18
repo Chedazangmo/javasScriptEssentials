6cdb66deea799bd8a4993724d979295c5661afd4
chedazangmo <02220137.cst@rub.edu.bt>
2026-03-18 13:08:31 -0400

    followers count

---
+++ followersCount/followersCount.js
@@ -0,0 +1,30 @@
+let count=0;
+
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count;
+}
+
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function checkCountValue(){
+    if (count==10){
+        alert("Your Instagram post gained 10 followers! Congratulation!");
+
+    }else if (count==20){
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+
+
+}
+
+function resetCount(){
+   count=0;
+   displayCount();
+   alert("Your follwers has been reset to zero !");
+}
+
